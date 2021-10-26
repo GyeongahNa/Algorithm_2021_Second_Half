ebdcbbfcd790bdce8f2a8503cc8ce9ce2b5dd80d
GyeongahNa <luckyrookie@khu.ac.kr>
2021-10-26 13:30:47 +0900

    Prompt412 실버

---
+++ 20211026/12796_나의행렬곱셈답사기.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm> 
+#include <cstring>
+#include <vector>
+#include <cmath>
+#include <tuple>
+#include <queue>
+
+using namespace std;
+using ii = pair<int, int>;
+using iii = tuple<int, int, int>;
+
+int main() {
+
+    int K;
+    scanf("%d", &K);
+
+    cout<<3<<endl;
+    cout<<"1 1 1 "<<K+1<<endl;
+    return 0;
+}
